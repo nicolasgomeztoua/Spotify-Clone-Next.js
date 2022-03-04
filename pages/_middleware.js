71e42f2fd4915e67fa690189cd6cc98145bff649
nicolasgomeztoua <nicolasnourgt@gmail.com>
2022-03-04 10:52:25 +0100

    adding login url

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -13,6 +13,6 @@ export async function middleware(req, res, next) {
   //if no token, take em to login.
 
   if (!token && pathname !== "/login") {
-    return NextResponse.redirect("http://localhost:3000/login");
+    return NextResponse.redirect(process.env.LOGIN_URL);
   }
 } 
