2c164d87ba5396b719bdbae88bd2dcea7646f281
nicolasgomeztoua <nicolasnourgt@gmail.com>
2022-03-04 12:39:36 +0100

    weird tailwind bug in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    // Add extra paths here
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
